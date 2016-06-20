61b50dd60f6b6987cb42a530a1b52d08ec3930e2
Liam Hoekenga <liamr@umich.edu>
2016-06-20 11:16:47 -0400

    Finish undoing bad commit 0eb2a2bdd6a508916234b2c1cb95ccfea9857a59

--- cgi/uservar.h
+++
@@ -1,9 +0,0 @@
-struct uservarlist {
-    char                *uv_var;
-    char                *uv_value;
-    struct uservarlist  *uv_next;
-};
-
-struct uservarlist *uservar_new( );
-
-void uservar_dispose( struct uservarlist *l );
